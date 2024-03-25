1bdac82ff004c0ddd750d44574a931f256e2e7bd
otreblan <otreblain@gmail.com>
2024-03-25 15:37:15 -0500

    feat: Add download_dataset()

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python3
 
-print("Hello world")
+import torch
+
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+def download_dataset() -> None:
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+
+def main() -> None:
+    download_dataset()
+
+if __name__ == '__main__':
+    main()
